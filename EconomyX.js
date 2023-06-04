6ea2f470f763a954cb1cb68edb9ce50b1003f173
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2023-06-04 15:42:24 +0700

    Update

--- EconomyX.js
+++ EconomyX.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getProcessedTags = void 0;
 const economy_x_1 = require("@bdsx/economy-x");
+const src_1 = require("@bdsx/economy-x/src");
 function getProcessedTags(player) {
     return [
         ["{money}", economy_x_1.EconomyX.getMoney(player)],
+        ["{currency}", src_1.EconomyConfig.getCurrency()],
     ];
 }
 exports.getProcessedTags = getProcessedTags;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRWNvbm9teVguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJFY29ub215WC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQ0FBMkM7QUFFM0MsU0FBZ0IsZ0JBQWdCLENBQUMsTUFBYztJQUMzQyxPQUFPO1FBQ0gsQ0FBQyxTQUFTLEVBQUUsb0JBQVEsQ0FBQyxRQUFRLENBQUMsTUFBTSxDQUFDLENBQUM7S0FDekMsQ0FBQztBQUNOLENBQUM7QUFKRCw0Q0FJQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRWNvbm9teVguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJFY29ub215WC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQ0FBMkM7QUFDM0MsNkNBQW9EO0FBRXBELFNBQWdCLGdCQUFnQixDQUFDLE1BQWM7SUFDM0MsT0FBTztRQUNILENBQUMsU0FBUyxFQUFFLG9CQUFRLENBQUMsUUFBUSxDQUFDLE1BQU0sQ0FBQyxDQUFDO1FBQ3RDLENBQUMsWUFBWSxFQUFFLG1CQUFhLENBQUMsV0FBVyxFQUFFLENBQUM7S0FDOUMsQ0FBQztBQUNOLENBQUM7QUFMRCw0Q0FLQyJ9
--- EconomyX.ts
+++ EconomyX.ts
@@ -1,9 +1,11 @@
 import { Player } from "bdsx/bds/player";
 import { AddonData } from "../src/addon";
 import { EconomyX } from "@bdsx/economy-x";
+import { EconomyConfig } from "@bdsx/economy-x/src";
 
 export function getProcessedTags(player: Player): AddonData[] {
     return [
         ["{money}", EconomyX.getMoney(player)],
+        ["{currency}", EconomyConfig.getCurrency()],
     ];
 }
