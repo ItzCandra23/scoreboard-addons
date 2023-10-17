5e70012f9dab19f3a0df07425184b2918365a99c
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2023-10-17 10:41:35 +0700

    Add files via upload

--- EconomyX.js
+++ EconomyX.js
@@ -2,12 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getProcessedTags = void 0;
 const economy_x_1 = require("@bdsx/economy-x");
-const src_1 = require("@bdsx/economy-x/src");
 function getProcessedTags(player) {
     return [
-        ["{money}", economy_x_1.EconomyX.getMoney(player)],
-        ["{currency}", src_1.EconomyConfig.getCurrency()],
+        ["{money}", economy_x_1.EconomyX.getMoney(player).toLocaleString()],
+        ["{currency}", economy_x_1.EconomyX.currency()],
     ];
 }
 exports.getProcessedTags = getProcessedTags;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRWNvbm9teVguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJFY29ub215WC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQ0FBMkM7QUFDM0MsNkNBQW9EO0FBRXBELFNBQWdCLGdCQUFnQixDQUFDLE1BQWM7SUFDM0MsT0FBTztRQUNILENBQUMsU0FBUyxFQUFFLG9CQUFRLENBQUMsUUFBUSxDQUFDLE1BQU0sQ0FBQyxDQUFDO1FBQ3RDLENBQUMsWUFBWSxFQUFFLG1CQUFhLENBQUMsV0FBVyxFQUFFLENBQUM7S0FDOUMsQ0FBQztBQUNOLENBQUM7QUFMRCw0Q0FLQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiRWNvbm9teVguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJFY29ub215WC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFFQSwrQ0FBMkM7QUFFM0MsU0FBZ0IsZ0JBQWdCLENBQUMsTUFBYztJQUMzQyxPQUFPO1FBQ0gsQ0FBQyxTQUFTLEVBQUUsb0JBQVEsQ0FBQyxRQUFRLENBQUMsTUFBTSxDQUFDLENBQUMsY0FBYyxFQUFFLENBQUM7UUFDdkQsQ0FBQyxZQUFZLEVBQUUsb0JBQVEsQ0FBQyxRQUFRLEVBQUUsQ0FBQztLQUN0QyxDQUFDO0FBQ04sQ0FBQztBQUxELDRDQUtDIn0=
--- EconomyX.ts
+++ EconomyX.ts
@@ -1,11 +1,10 @@
 import { Player } from "bdsx/bds/player";
 import { AddonData } from "../src/addon";
 import { EconomyX } from "@bdsx/economy-x";
-import { EconomyConfig } from "@bdsx/economy-x/src";
 
 export function getProcessedTags(player: Player): AddonData[] {
     return [
-        ["{money}", EconomyX.getMoney(player)],
-        ["{currency}", EconomyConfig.getCurrency()],
+        ["{money}", EconomyX.getMoney(player).toLocaleString()],
+        ["{currency}", EconomyX.currency()],
     ];
 }
